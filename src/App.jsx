48cbd9eacb7f6b7b6c2aa4c0f86d205777b79afb
OpeltsUnlimited <patrick@opelt.co.at>
2026-01-04 17:54:32 +0100

    Simple interpret and show

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Stack, Nav} from 'react-bootstrap';
 import './App.css'
 
 import MainModell from './MainModell.jsx'
@@ -6,14 +7,17 @@ import MainModell from './MainModell.jsx'
 var m = new MainModell()
 
 function App() { 
-  
+
   const AppState = Object.freeze({
       Init:   0,
       Loading:   1,
-      Finished: 2,
+      Interpreting: 2,
+      Finished: 3,
   });
 
   const [appState, setAppState] = useState(AppState.Init);
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const [selectedType, setSelectedType] = useState(0);
 
    const initApp = async () => {
     console.log("initApp", appState)
@@ -25,13 +29,16 @@ function App() {
         await m.load();
         setAppState(appState + 1)
         return
+      case AppState.Interpreting:
+        await m.interpret();
+        setAppState(appState + 1)
+        return
   }
   };
 
   useEffect(() => {
     initApp();
   }, [appState]);
-
 
   switch(appState) {
   case AppState.Init:
@@ -47,8 +54,8 @@ function App() {
             INIT
       </div>
     </div>
-    
     )
+
   case AppState.Loading:
     return (
     <div className="App">
@@ -62,8 +69,23 @@ function App() {
             LOADING
       </div>
     </div>
-    
     )
+    
+  case AppState.Interpreting:
+    return (
+    <div className="App">
+      <div  className="navbar bg-dark text-white p-2 ">
+        <a className="navbar-brand text-white" href="#">
+          <img src="https://github.com/WFCD/warframe-items/blob/master/data/img/clan-sigil-b17cd6bed0.png?raw=true" alt="logo"  width="30" height="30" className="d-inline-block align-top"/> 
+          Checklist V0!
+        </a>
+      </div>
+      <div className="container list-group mt-3">
+            Interpreting
+      </div>
+    </div>
+    )
+
   case AppState.Finished:
     return (
     <div className="App">
@@ -73,7 +95,33 @@ function App() {
           Checklist V0!
         </a>
       </div>
-      <div>A<pre>{JSON.stringify(m.data, null, 2) }</pre>A</div>
+      <Stack direction="horizontal" className="align-items-start">
+      <div className="flex-column list-group mt-3">
+        {Array.from(m.categorys).map(([key, value]) => (
+          <Button 
+          variant={selectedCategory == value ? 'primary' : 'outline-primary'}
+          onClick={() => {setSelectedCategory(value)
+          setSelectedType(0)}}>
+            {key}
+          </Button>
+        ))}
+        </div>
+      <div className="flex-column list-group mt-3">
+        {Array.from(selectedCategory).map(([key, value]) => (
+          <Button 
+          variant={selectedType == value ? 'primary' : 'outline-primary'}
+          onClick={() =>setSelectedType(value)}>
+            {key}
+          </Button>
+        ))}
+        </div>
+        <div className="flex-column list-group mt-3">
+          { !selectedType ? null : selectedType.map((value) => (
+            <div>{value["name"]}</div>
+          ))
+          }
+        </div>
+      </Stack>
     </div>
     )
   }
--- src/MainModell.jsx
+++ src/MainModell.jsx
@@ -3,44 +3,47 @@ import js from "@eslint/js";
 class MainModell {
     constructor() {
         this.data = 0
+
+        this.objects = new Map() // unique_name / object
+        this.categorys = new Map() // Map(category, Map(type, List(object, Refference)))
     }
     
     async load() {
         try {
             var response = await fetch("https://raw.githubusercontent.com/WFCD/warframe-items/refs/heads/master/data/json/All.json");
             this.data = await response.json();
-
-            const categorys =  new Set();
-            const types =  new Set();
-            const tree = {}
-
-            var tst = 0;
-            for(var i = 0; i < this.data.length; i++) {
-                const j = this.data[i]
-                const cat = j["category"]
-                const typ = j["type"]
-                categorys.add(cat)
-                types.add(typ)
-
-                var tree_elem = tree[cat]
-                if (!tree_elem) {
-                    tree_elem = new Set()
-                    tree[cat] = tree_elem
-                }
-                tree_elem.add(typ)
-                tst ++;
-            }
-            console.log("tst", tst);
-            console.log("Cat", categorys);
-            console.log("Typ", types);
-            console.log("tree", tree);
-            console.log("data", this.data);
-            
-
         } catch (e) {
-            console.log('Booo', e);
+            console.log('Error', e);
+        }
+    }
+    
+    async interpret() {
+        for(var i = 0; i < this.data.length; i++) {
+            const j = this.data[i]
+            const cat = j["category"]
+            const typ = j["type"]
+            const uniqueName = j["uniqueName"]
+
+            var obj = new Map()
+            obj["category"] = cat
+            obj["type"] = typ
+            obj["name"] = j["name"]
+
+            if (!this.categorys.has(cat) ) {
+                this.categorys.set(cat, new Map());
+            }
+            var map_cat = this.categorys.get(cat)
+
+            if (!map_cat.has(typ) ) {
+                map_cat.set(typ, new Array());
+            }
+            var map_typ = map_cat.get(typ)
+            map_typ.push(obj)
+
+            this.objects.set(uniqueName, obj)
         }
     }
 }
+
   
 export default MainModell;
