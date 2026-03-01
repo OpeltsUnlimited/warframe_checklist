d3a33d9c01ec3eb332bf9b30637f3089fd3d74ab
OpeltsUnlimited <patrick@opelt.co.at>
2026-03-01 19:34:58 +0100

    Problem with root poath of build
    Worked lokaly but not on github-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // relative asset path, schould work lokally and on ghpages?
   plugins: [react()],
 })
